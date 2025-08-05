ca339695f15460866cfb8dca9e3a6cc0e9f60ed8
Hasa24 <109957706+Hasa24@users.noreply.github.com>
2025-08-05 13:57:26 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Serve static files from public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+  console.log('User connected');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg); // Broadcast to all users
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
